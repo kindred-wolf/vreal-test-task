238f595ce5a05e29e6cd61d837bd32e23605a011
Vadym Chekhlatyi <94396916+vadym-chekhlatyi@users.noreply.github.com>
2024-06-02 23:56:59 +0300

    Swagger module imported

--- blog-api/src/main.ts
+++ blog-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './AppModule'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Documentation for VReal test task')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000)
 }
 bootstrap()
